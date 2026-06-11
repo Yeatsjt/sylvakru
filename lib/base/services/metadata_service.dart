9b592adc378bcb1a7172be4e451457f8eb8befd0
AfalpHy <736353503@qq.com>
2026-06-11 16:55:56 +0800

    perf: optimize image decoding

--- lib/base/services/metadata_service.dart
+++ lib/base/services/metadata_service.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
 import 'dart:typed_data';
+import 'dart:ui' as ui;
 
 import 'package:audio_tags_lofty/audio_tags_lofty.dart';
 import 'package:flutter/material.dart';
-import 'package:image/image.dart' as image;
 import 'package:sylvakru/base/my_audio_metadata.dart';
 import 'package:sylvakru/base/services/emby_client.dart';
 import 'package:sylvakru/base/services/navidrome_client.dart';
@@ -72,44 +72,65 @@ Future<Color> computeCoverArtColor(MyAudioMetadata? song) async {
     song?.coverArtColor = Colors.grey;
     return Colors.grey;
   }
+  final codec = await ui.instantiateImageCodec(
+    bytes,
+    targetWidth: 20,
+    targetHeight: 20,
+  );
 
-  final decoded = image.decodeImage(bytes);
-  if (decoded == null) {
-    song?.coverArtColor = Colors.grey;
+  final frameInfo = await codec.getNextFrame();
+  final image = frameInfo.image;
+
+  final byteData = await image.toByteData(format: ui.ImageByteFormat.rawRgba);
+
+  if (byteData == null) {
     return Colors.grey;
   }
 
-  // simple average of top pixels
-  double r = 0, g = 0, b = 0, count = 0;
-  for (int y = 0; y < decoded.height; y += 5) {
-    for (int x = 0; x < decoded.width; x += 5) {
-      final pixel = decoded.getPixel(x, y);
-      if (pixel.a == 0) {
-        r += 128;
-        g += 128;
-        b += 128;
-      } else {
-        r += pixel.r.toDouble();
-        g += pixel.g.toDouble();
-        b += pixel.b.toDouble();
-      }
+  final Uint8List buffer = byteData.buffer.asUint8List();
 
-      count++;
+  double r = 0;
+  double g = 0;
+  double b = 0;
+  int count = 0;
+  for (int i = 0; i < buffer.length; i += 4) {
+    final int red = buffer[i];
+    final int green = buffer[i + 1];
+    final int blue = buffer[i + 2];
+    final int alpha = buffer[i + 3];
+
+    if (alpha == 0) {
+      r += 128;
+      g += 128;
+      b += 128;
+    } else {
+      r += red;
+      g += green;
+      b += blue;
     }
+    count++;
   }
+
+  if (count == 0) return Colors.grey;
+
   r /= count;
   g /= count;
   b /= count;
   final color = Color.fromARGB(255, r.toInt(), g.toInt(), b.toInt());
   song!.coverArtColor = color;
 
-  int luminance = image.getLuminanceRgb(r, g, b).toInt();
-  int maxLuminace = 200;
-  if (luminance > maxLuminace) {
-    r -= luminance - maxLuminace;
-    g -= luminance - maxLuminace;
-    b -= luminance - maxLuminace;
-    song.lowerLuminance = Color.fromARGB(255, r.toInt(), g.toInt(), b.toInt());
+  final luminance = 0.299 * r + 0.587 * g + 0.114 * b;
+  const maxLuminance = 200;
+
+  if (luminance > maxLuminance) {
+    final reduction = luminance.toInt() - maxLuminance;
+
+    song.lowerLuminance = Color.fromARGB(
+      255,
+      (r - reduction).clamp(0, 255).toInt(),
+      (g - reduction).clamp(0, 255).toInt(),
+      (b - reduction).clamp(0, 255).toInt(),
+    );
   }
   return color;
 }
