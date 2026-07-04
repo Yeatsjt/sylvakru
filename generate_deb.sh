cb60387957ea64d4ab70667a4eb9b099e465ea76
AfalpHy <736353503@qq.com>
2026-07-04 13:15:31 +0800

    chore: bump version to 3.4.5

--- generate_deb.sh
+++ generate_deb.sh
@@ -3,7 +3,7 @@
 # ---------- CONFIG ----------
 BINARY_NAME="sylvakru"
 APP_NAME="Sylvakru"
-APP_VERSION="3.4.4"
+APP_VERSION="3.4.5"
 MAINTAINER="AfalpHy"
 
 BUNDLE_DIR="build/linux/x64/release/bundle"
